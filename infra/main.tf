557e751063023db7a4a176c57df39dc301073ebe
José Henrique <henriquesantoscjr2@hotmail.com>
2024-11-29 22:34:25 -0300

    Ajuste Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -13,14 +13,14 @@ resource "aws_sqs_queue" "my_queue" {
 resource "aws_lambda_permission" "allow_sqs" {
   statement_id  = "AllowSQSTrigger"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_pedido.function_name
+  function_name = data.aws_lambda_function.lambda_pedido.function_name
   principal     = "sqs.amazonaws.com"
   source_arn    = aws_sqs_queue.my_queue.arn
 }
 
 resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
   event_source_arn = aws_sqs_queue.my_queue.arn
-  function_name    = aws_lambda_function.lambda_pedido.arn
+  function_name    = data.aws_lambda_function.lambda_pedido.arn
   batch_size       = 10
   enabled          = true
 }
