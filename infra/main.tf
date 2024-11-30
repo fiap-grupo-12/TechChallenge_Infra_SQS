81a462df6135e950df674901538a88f0a11fa623
José Henrique <henriquesantoscjr2@hotmail.com>
2024-11-29 22:42:27 -0300

    Ajuste terraform

--- infra/main.tf
+++ infra/main.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_permission" "allow_sqs" {
 
 resource "aws_lambda_event_source_mapping" "sqs_to_lambda" {
   event_source_arn = aws_sqs_queue.my_queue.arn
-  function_name    = data.aws_lambda_function.lambda_pedido.arn
+  function_name    = data.aws_lambda_function.lambda_pedido.function_name
   batch_size       = 10
   enabled          = true
 }
